5bb6b374ecaf8f40a4baf0e41c6255dc959736a8
Jonathan Kitt <jonathan.kitt@protonmail.com>
2022-07-21 13:20:02 +0000

    version 0.0.3

--- R/datasets.R
+++ R/datasets.R
@@ -1,4 +1,4 @@
-#' List of episodes
+#' List of Dr Who episodes
 #'
 #' List of all episodes from the revived Doctor Who era (2005-present)
 #'
@@ -19,16 +19,17 @@
 #' }
 #'
 #' @import dplyr
+#' @import magrittr
 #'
 #' @source \url{https://en.wikipedia.org/wiki/List_of_Doctor_Who_episodes_(2005-present)}
 #'
 #' @examples
 #' library(dplyr)
-#' episodes %>%
+#' drwho_episodes %>%
 #'     filter(type == "special")
-"episodes"
+"drwho_episodes"
 
-#' List of directors
+#' List of Dr Who directors
 #'
 #' List of directors for all episodes from the revived Doctor Who era (2005-present)
 #'
@@ -44,13 +45,13 @@
 #'
 #' @examples
 #' library(dplyr)
-#' directors %>%
+#' drwho_directors %>%
 #'     count(director) %>%
 #'     arrange(desc(n)) %>%
 #'     head(5)
-"directors"
+"drwho_directors"
 
-#' List of writers
+#' List of Dr Who writers
 #'
 #' List of writers for all episodes from the revived Doctor Who era (2005-present)
 #'
@@ -66,8 +67,80 @@
 #'
 #' @examples
 #' library(dplyr)
-#' writers %>%
+#' drwho_writers %>%
 #'     count(writer) %>%
 #'     arrange(desc(n)) %>%
 #'     head(5)
-"writers"
+"drwho_writers"
+
+#' List of Torchwood episodes
+#'
+#' List of all episodes from the Torchwood series (2006-2011)
+#'
+#' @format This data frame contains the following columns:
+#' \describe{
+#'   \item{\code{season_number}}{Season number}
+#'   \item{\code{serial_title}}{Serial title if available}
+#'   \item{\code{story_number}}{Story number}
+#'   \item{\code{episode_number}}{Episode number in season}
+#'   \item{\code{episode_title}}{Episode Title}
+#'   \item{\code{first_aired}}{Date the episode first aired in the U.K.}
+#'   \item{\code{production_code}}{Episode's production code if available}
+#'   \item{\code{uk_viewers}}{Number of U.K. viewers (millions)}
+#'   \item{\code{duration}}{Episode's duration in minutes}
+#' }
+#'
+#' @import dplyr
+#' @import magrittr
+#'
+#' @source \url{https://en.wikipedia.org/wiki/List_of_Torchwood_episodes}
+#'
+#' @examples
+#' library(dplyr)
+#' torchwood_episodes %>%
+#'     filter(uk_viewers >= 5)
+"torchwood_episodes"
+
+#' List of Torchwood directors
+#'
+#' List of directors for all episodes from Torchwood series (2006-2011)
+#'
+#' @format This data frame contains the following columns:
+#' \describe{
+#'   \item{\code{story_number}}{Story number}
+#'   \item{\code{director}}{Episode's director}
+#' }
+#'
+#' @import dplyr
+#'
+#' @source \url{https://en.wikipedia.org/wiki/List_of_Torchwood_episodes}
+#'
+#' @examples
+#' library(dplyr)
+#' torchwood_directors %>%
+#'     count(director) %>%
+#'     arrange(desc(n)) %>%
+#'     head(5)
+"torchwood_directors"
+
+#' List of Torchwood writers
+#'
+#' List of directors for all episodes from Torchwood series (2006-2011)
+#'
+#' @format This data frame contains the following columns:
+#' \describe{
+#'   \item{\code{story_number}}{Story number}
+#'   \item{\code{writer}}{Episode's writer}
+#' }
+#'
+#' @import dplyr
+#'
+#' @source \url{https://en.wikipedia.org/wiki/List_of_Torchwood_episodes}
+#'
+#' @examples
+#' library(dplyr)
+#' torchwood_writers %>%
+#'     count(writer) %>%
+#'     arrange(desc(n)) %>%
+#'     head(5)
+"torchwood_writers"
